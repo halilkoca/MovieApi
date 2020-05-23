b5bbee868379e797b7aa0290c6ba0c5f275ac79f
Halil Koca <ihalilkoca@gmail.com>
2020-05-23 10:38:35 +0300

    added api, core, data, entity projects

---
+++ App/App.Api/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
